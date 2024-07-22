dd4a988c59c4a000929898c5041d7777aaed2769
manatago <satoshi@tagomori.biz>
2024-07-22 13:58:37 +0900

    0.1.0.1

--- lib/TagoStripe/Product.rb
+++ lib/TagoStripe/Product.rb
@@ -1,7 +1,7 @@
 require 'stripe'
 module TagoStripe
     class Product
-        def getList
+        def self.getList
             Stripe.api_key = ENV['STRIPE_SECRET_KEY']
             products = Stripe::Product.list()
         end
